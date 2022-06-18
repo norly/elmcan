norly/elmcan

commit 37111be717212b8c7779978c0385393c2d51747d

Stop leaking SKBs in elm327_parse_frame()
SKBs need to be used or freed before returning, no exceptions.

module/can327.c
@@ -440,6 +440,7 @@ static int elm327_parse_frame(struct can327 *elm, size_t len)
 		/* The line is likely garbled anyway, so bail.
 		 * The main code will restart listening.
 		 */
+		kfree_skb(skb);
 		return -ENODATA;
 	}
 
@@ -458,6 +459,7 @@ static int elm327_parse_frame(struct can327 *elm, size_t len)
 		/* This is not a well-formatted data line.
 		 * Assume it's an error message.
 		 */
+		kfree_skb(skb);
 		return -ENODATA;
 	}
 
@@ -465,6 +467,7 @@ static int elm327_parse_frame(struct can327 *elm, size_t len)
 		/* The line is too short to be a valid frame hex dump.
 		 * Something interrupted the hex dump or it is invalid.
 		 */
+		kfree_skb(skb);
 		return -ENODATA;
 	}
 
@@ -516,6 +519,7 @@ static int elm327_parse_frame(struct can327 *elm, size_t len)
 		 * However, this will correctly drop the state machine back into
 		 * command mode.
 		 */
+		kfree_skb(skb);
 		return -ENODATA;
 	}
 

commit e1a93edd065ef8888d3043283628bb77a06d5c96

Don't kfree_skb() after submitting the repurposed SKB
If can327 locks up your machine, but only in very specific situations,
this is probably why. The memory leak fix went too far, and I missed
the call to can327_feed_frame_to_netdev() before a return -ENODATA.

Fixes: 37111be717212b8c7779978c0385393c2d51747d

module/can327.c
@@ -518,7 +518,6 @@ static int can327_parse_frame(struct can327 *elm, size_t len)
 		 * However, this will correctly drop the state machine back into
 		 * command mode.
 		 */
-		kfree_skb(skb);
 		return -ENODATA;
 	}
 